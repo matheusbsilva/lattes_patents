58b3a26bce8331b7683a680d708cc342ad9d4dfd
naiieandrade <andradenaiara@hotmail.com>
2018-10-19 22:09:41 -0300

    Update script to read attributes in XML

--- read_patents.R
+++ read_patents.R
@@ -1,26 +1,52 @@
+#install.packages("xml2")
+#install.packages("dplyr")
+#install.packages("XML")
+
 library(xml2)
 library(dplyr)
 library(XML)
+library(tidyverse)
 
 # Define diretório padrão
-setwd("~/Documentos/ds4all/gcee/")
+# setwd("~/Documentos/ds4all/gcee/")
+# setwd("~/Documentos/UNB/2 18/ds/git/lattes_patents/")
 
 # Caminho de todos os curriculos
+# all.xmls <- list.files("curriculos/", full = TRUE)
 all.xmls <- list.files("teste/", full = TRUE)
 lista.patentes <- list()
 
 for (path.xml in all.xmls) {
-  lista.xml <- xmlToList(path.xml)
+  lista.xml <- read_xml(path.xml)
   
-  if(is.null(lista.xml[["PRODUCAO-TECNICA"]][["PATENTE"]])) next
+  # Busca pela TAG patente em todo o xml
+  patentes <- xml_find_all(lista.xml, "//PATENTE")
   
-  for (item in names(lista.xml[["PRODUCAO-TECNICA"]])) {
-    if (item == "PATENTE") {                        
-      lista.patentes[lista.xml[[".attrs"]][["NUMERO-IDENTIFICADOR"]]] <- list(lista.xml[["PRODUCAO-TECNICA"]][item])
-    }
+  #TODO: tratar casos em que o atributo NUMERO-IDENTIFICADOR está vazio
+  if (length(patentes) != 0) {
+    lista.patentes[[xml_attrs(lista.xml)[["NUMERO-IDENTIFICADOR"]]]] <- as_list(patentes)
   }
-  
-  #lista.patentes[lista.xml[[".attrs"]][["NUMERO-IDENTIFICADOR"]]] <- list(lista.xml[["PRODUCAO-TECNICA"]])
-  
 }
+
+
+
+
+# Teste com um arquivo
+xmlobj_ds <- read_xml("teste/0021199964477362.xml")
+
+# Assim converte para DataFrame, mas tem que ver porque não ta vindo todas as informações corretas, como o numero de sequencia e a quantidade e os nomes dos autores
+
+df_items_ds <- data.frame( 
+  seq_prod   = xml_find_all( xmlobj_ds, ".//PATENTE" ) %>% xml_attr( "SEQUENCIA-PRODUCAO" ),
+  titulo = xml_find_all( xmlobj_ds, ".//PATENTE/DADOS-BASICOS-DA-PATENTE" ) %>%  xml_attr( "TITULO" ),
+  ano = xml_find_all( xmlobj_ds, ".//PATENTE/DADOS-BASICOS-DA-PATENTE" ) %>% xml_attr("ANO-DESENVOLVIMENTO"),
+  categoria = xml_find_all( xmlobj_ds, ".//PATENTE/DETALHAMENTO-DA-PATENTE" ) %>% xml_attr("CATEGORIA"),
+  autor1 = xml_find_all( xmlobj_ds, ".//PATENTE/AUTORES" ) %>% xml_attr("NOME-COMPLETO-DO-AUTOR"),
+  stringsAsFactors = FALSE )
+
+# Assim tem que colocar em lista os dados pedidos: autores, ids
+
+oddsetds <- xml_find_all(xmlobj_ds, ".//PATENTE") %>% 
+  xml_children() %>% map(xml_attrs) %>% map_df(~as.list(.))
+
 
