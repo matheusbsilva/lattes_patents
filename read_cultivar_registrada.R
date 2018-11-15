c970c8c670a15f7397d3159cd2269c1b32e5b206
naiieandrade <andradenaiara@hotmail.com>
2018-11-15 12:37:56 -0200

    Read registered and protected cultivate and industrial draw

---
+++ read_cultivar_registrada.R
@@ -0,0 +1,142 @@
+setwd("~/Documentos/UNB/2-18/ds/git/lattes_patents/curriculos")
+
+
+library(XML)
+library(xlsx)
+library(dplyr)
+
+
+#################################################
+# Função para validar xml attr do E-lattes
+Validate <- function (element, field) {
+  result <- ""
+  
+  tryCatch(
+    result <- xmlGetAttr(element, field)  ,
+    error = function(e) {
+      
+    }
+  )
+  
+  if (is.null(result) || is.na(result))
+    result <- ""
+  result
+}
+
+# Função para ponto vírgula e espaço
+SemiColon <- function (field, target){ 
+  if (is.null(field) || field == "") {
+    target <- paste(target, "VAZIO", sep = "; ")
+    
+  }
+  else{
+    if (target != "VAZIO") {
+      target <- paste(target, field, sep = "; ")
+    }
+    else{
+      target <- paste(field, sep = "; ")
+    }
+  }
+  
+  target
+}
+################################################
+
+all.xmls <- list.files(".")
+#path <- "0000325690951570.xml"
+df_pat <- data.frame()
+
+for (path.xml in all.xmls) {
+  file <- xmlParse(path.xml)
+  #file <- xmlParse(path)
+  root <- xmlRoot(file)
+  df_aux <- NULL
+  
+  cultivares <- root[["PRODUCAO-TECNICA"]]["CULTIVAR-REGISTRADA", all = TRUE]
+  nome_autores <- "VAZIO"
+  nome_citacao_autores <- "VAZIO"
+  ordem_autores <- "VAZIO"
+  id_autores <- "VAZIO"
+  
+  for (cultivar in cultivares) {
+    cultivar_autores <- cultivar["AUTORES", all = TRUE]
+    for (autor in cultivar_autores) {
+      nome_completo <- Validate(autor, "NOME-COMPLETO-DO-AUTOR")
+      nome_citacao <- Validate(autor, "NOME-PARA-CITACAO")
+      ordem <- Validate(autor, "ORDEM-DE-AUTORIA")
+      idcnpq <- Validate(autor, "NRO-ID-CNPQ")
+      
+      nome_autores <- SemiColon(nome_completo, nome_autores)
+      nome_citacao_autores <- SemiColon(nome_citacao, nome_citacao_autores)
+      ordem_autores <- SemiColon(ordem, ordem_autores)
+      id_autores <- SemiColon(idcnpq, id_autores)
+    }
+    
+    
+    # variáveis das tags
+    
+    DADOS_BASICOS <- "DADOS-BASICOS-DA-CULTIVAR"
+    DETALHAMENTO <- "DETALHAMENTO-DA-CULTIVAR"
+    
+    df_aux <- data.frame(
+      # Informações básicas
+      id_lattes = gsub(".xml", "", path.xml),
+      nome_completo = Validate(root[["DADOS-GERAIS"]], "NOME-COMPLETO"),
+      id_patente = Validate(cultivar[[DETALHAMENTO]][["REGISTRO-OU-PATENTE"]], "CODIGO-DO-REGISTRO-OU-PATENTE"),
+      
+      # TAG: DADOS-BASICOS-DA-CULTIVAR-REGISTRADA
+      denominacao = Validate(cultivar[[DADOS_BASICOS]], "DENOMINACAO"),
+      ano_solicitacao = Validate(cultivar[[DADOS_BASICOS]], "ANO-SOLICITACAO"),
+      pais = Validate(cultivar[[DADOS_BASICOS]], "PAIS"),
+      flag_relevancia = Validate(cultivar[[DADOS_BASICOS]], "FLAG-RELEVANCIA"),
+      denominacao_ingles = Validate(cultivar[[DADOS_BASICOS]], "DENOMINACAO-INGLES"),
+      flag_potencial_inovacao = Validate(cultivar[[DADOS_BASICOS]], "FLAG-POTENCIAL-INOVACAO"),
+      
+      # TAG: DETALHAMENTO-DA-CULTIVAR-REGISTRADA
+      finalidade = Validate(cultivar[[DETALHAMENTO]], "FINALIDADE"),
+      instituicao_financiadora = Validate(cultivar[[DETALHAMENTO]], "INSTITUICAO-FINANCIADORA"),
+      finalidade_ingles = Validate(cultivar[[DETALHAMENTO]], "FINALIDADE-INGLES"),
+      
+      # TAG: DETALHAMENTO-DA-CULTIVAR-REGISTRADA > REGISTRO-OU-PATENTE
+      #WARN: Informação duplicada, TITULO-PATENTE
+      titulo_patente_reg = Validate(cultivar[[DETALHAMENTO]][["REGISTRO-OU-PATENTE"]], "TITULO-PATENTE"),
+      data_pedido_de_deposito = Validate(cultivar[[DETALHAMENTO]][["REGISTRO-OU-PATENTE"]], "DATA-PEDIDO-DE-DEPOSITO"),
+      data_pedido_de_exame = Validate(cultivar[[DETALHAMENTO]][["REGISTRO-OU-PATENTE"]], "DATA-DE-PEDIDO-DE-EXAME"),
+      data_de_concessao = Validate(cultivar[[DETALHAMENTO]][["REGISTRO-OU-PATENTE"]], "DATA-DE-CONCESSAO"),
+      instituicao_deposito_registro = Validate(cultivar[[DETALHAMENTO]][["REGISTRO-OU-PATENTE"]], "INSTITUICAO-DEPOSITO-REGISTRO"),
+      numero_deposito_pct = Validate(cultivar[[DETALHAMENTO]][["REGISTRO-OU-PATENTE"]], "NUMERO-DEPOSITO-PCT"),
+      formato_data_deposito_pct = Validate(cultivar[[DETALHAMENTO]][["REGISTRO-OU-PATENTE"]], "FORMATO-DATA-DEPOSITO-PCT"),
+      data_deposito_pct = Validate(cultivar[[DETALHAMENTO]][["REGISTRO-OU-PATENTE"]], "DATA-DEPOSITO-PCT"),
+      nome_titular = Validate(cultivar[[DETALHAMENTO]][["REGISTRO-OU-PATENTE"]], "NOME-DO-TITULAR"),
+      nome_depositante = Validate(cultivar[[DETALHAMENTO]][["REGISTRO-OU-PATENTE"]], "NOME-DO-DEPOSITANTE"),
+      sta_validacao = Validate(cultivar[[DETALHAMENTO]][["REGISTRO-OU-PATENTE"]], "STA-VALIDADO"),
+      
+      # TAG: AREAS-DO-CONHECIMENTO 
+      # TODO: se tiver mais de uma?
+      nome_grande_area_do_conhecimento_1 = Validate(cultivar[["AREAS-DO-CONHECIMENTO"]][["AREA-DO-CONHECIMENTO-1"]], "NOME-GRANDE-AREA-DO-CONHECIMENTO"),
+      nome_da_area_do_conhecimento_1 = Validate(cultivar[["AREAS-DO-CONHECIMENTO"]][["AREA-DO-CONHECIMENTO-1"]], "NOME-DA-AREA-DO-CONHECIMENTO"),
+      nome_da_sub_area_do_conhecimento_1 = Validate(cultivar[["AREAS-DO-CONHECIMENTO"]][["AREA-DO-CONHECIMENTO-1"]], "NOME-DA-SUB-AREA-DO-CONHECIMENTO"),
+      nome_da_especialidade_1 = Validate(cultivar[["AREAS-DO-CONHECIMENTO"]][["AREA-DO-CONHECIMENTO-1"]], "NOME-DA-ESPECIALIDADE"),
+
+      nome_grande_area_do_conhecimento_2 = Validate(cultivar[["AREAS-DO-CONHECIMENTO"]][["AREA-DO-CONHECIMENTO-2"]], "NOME-GRANDE-AREA-DO-CONHECIMENTO"),
+      nome_da_area_do_conhecimento_2 = Validate(cultivar[["AREAS-DO-CONHECIMENTO"]][["AREA-DO-CONHECIMENTO-2"]], "NOME-DA-AREA-DO-CONHECIMENTO"),
+      nome_da_sub_area_do_conhecimento_2 = Validate(cultivar[["AREAS-DO-CONHECIMENTO"]][["AREA-DO-CONHECIMENTO-2"]], "NOME-DA-SUB-AREA-DO-CONHECIMENTO"),
+      nome_da_especialidade_2 = Validate(cultivar[["AREAS-DO-CONHECIMENTO"]][["AREA-DO-CONHECIMENTO-2"]], "NOME-DA-ESPECIALIDADE"),
+      
+      # TAG: SETORES-DE-ATIVIDADE
+      setor_de_atividade_1 = Validate(cultivar[["SETORES-DE-ATIVIDADE"]], "SETOR-DE-ATIVIDADE-1"),
+      setor_de_atividade_2 = Validate(cultivar[["SETORES-DE-ATIVIDADE"]], "SETOR-DE-ATIVIDADE-2"),
+      setor_de_atividade_3 = Validate(cultivar[["SETORES-DE-ATIVIDADE"]], "SETOR-DE-ATIVIDADE-3"),
+      
+      
+      # AUTORES
+      nome_completo_autores = nome_autores,
+      nome_citacao_autores = nome_citacao_autores,
+      ordem_autoria_autores = I(list(ordem_autores)),
+      id_autores = I(list(id_autores)),
+      
+      stringsAsFactors = FALSE
+    )
+    df_pat <- rbind(df_pat, df_aux)
+  }
+}
