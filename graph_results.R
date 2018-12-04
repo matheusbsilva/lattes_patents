b55402f6b8c5ae76a9dd7d5a93a3fbd0ae4c0798
matheusbsilva <matheusbattista@hotmail.com>
2018-12-04 20:45:22 -0300

    refact graph generation

--- graph_results.R
+++ graph_results.R
@@ -2,70 +2,86 @@
 # Graph
 library(ggplot2)
 
-newers <- filter(result, ano_desenvolvimento >= 2010)
-incts <- data.frame(table(newers$inct))
-colnames(incts) <- c('inct', 'frequência')
+FilterGreaterThan1996 <- function(df){
+  greater_1996 <- filter(df, ano_desenvolvimento >= 1996)
+  greater_1996
+}
 
-result_after_2010 <- filter(result, ano_desenvolvimento >= 2010)
+FilterGreaterThan2010 <- function(df) {
+  result_after_2010 <- filter(df, ano_desenvolvimento >= 2010)
+  result_after_2010
+}
 
-greater_1996 <- filter(result, ano_desenvolvimento >= 1996)
-year_df <- data.frame(table(greater_1996$ano_desenvolvimento), stringsAsFactors = FALSE)
-colnames(year_df) <- c('ano', 'qtd_patentes')
+GraphByYear <- function(df) {
+  greater_1996 <- FilterGreaterThan1996(df)
+  year_df <- data.frame(table(greater_1996$ano_desenvolvimento), stringsAsFactors = FALSE)
+  colnames(year_df) <- c('ano', 'qtd_patentes')
+  
+  year_bar <- ggplot(year_df, aes(x=ano, y=qtd_patentes)) +
+    geom_bar(stat='identity', fill='#388E8E') +
+    labs(title = "Quantidade de patentes por ano", x = "Ano de desenvolvimento", y = "Número de patentes")
+  
+}
 
-# Graph by year
-year_bar <- ggplot(year_df, aes(x=ano, y=qtd_patentes)) +
-  geom_bar(stat='identity', fill='#388E8E') +
-  labs(title = "Quantidade de patentes por ano", x = "Ano de desenvolvimento", y = "Número de patentes")
+GraphByMeanYear <- function(df) {
+  greater_1996 <- FilterGreaterThan1996(df)
+  before_2010 <- filter(greater_1996, ano_desenvolvimento >= 2002 & ano_desenvolvimento < 2010)
+  after_2010 <- filter(greater_1996, ano_desenvolvimento >= 2010 & ano_desenvolvimento < 2018)
+  
+  before_2010 <- data.frame(table(before_2010$ano_desenvolvimento), stringsAsFactors = FALSE)
+  colnames(before_2010) <- c('ano', 'qtd_patentes')
+  
+  after_2010 <- data.frame(table(after_2010$ano_desenvolvimento), stringsAsFactors = FALSE)
+  colnames(after_2010) <- c('ano', 'qtd_patentes')
+  
+  means_years <- data.frame("year" = c("2002-2009","2010-2017"), "media" = c(mean(before_2010$qtd_patentes), mean(after_2010$qtd_patentes)))
+  
+  # Graph mean of years before and after 2010
+  mean_year_bar <- ggplot(means_years, aes(x=year, y=media)) +
+    geom_bar(stat='identity', fill='#388E8E') +
+    labs(title = "Média de patentes antes e após 2010", x = "Ano", y = "Média do número de patentes")
+  
+}
 
+GraphByCountry <- function(df) {
+  result_after_2010 <- FilterGreaterThan2010(df)
+  
+  # By country graph
+  country_df <- data.frame(table(result_after_2010$pais))
+  colnames(country_df) <- c('pais', 'qtd_patentes')
+  
+  filtered_countries <- filter(country_df, qtd_patentes > 1 & pais != 'Brasil')
+  country_bar <- ggplot(filtered_countries, aes(x=reorder(pais, qtd_patentes, sum), y=qtd_patentes)) +
+    coord_flip() +
+    scale_y_continuous(breaks = seq(0, 550, 25)) +
+    geom_bar(stat='identity', fill = '#0d627a') +
+    labs(title = "Patentes por países", x = "País", y = "Número de patentes")
+  
+}
 
-before_2010 <- filter(greater_1996, ano_desenvolvimento >= 2002 & ano_desenvolvimento < 2010)
-after_2010 <- filter(greater_1996, ano_desenvolvimento >= 2010 & ano_desenvolvimento < 2018)
+GraphByCategory <- function(df) {
+  result_after_2010 <- FilterGreaterThan2010(df)
+  # By category
+  pat_category <- data.frame(table(result_after_2010$categoria))
+  colnames(pat_category) <- c('categoria', 'qtd_patentes')
+  filtered_category <- filter(pat_category, qtd_patentes > 2 & categoria != "")
+  category_bar <- ggplot(filtered_category, aes(x=categoria, y=qtd_patentes)) +
+    geom_bar(stat='identity', fill = '#0d627a') +
+    labs(title = "Patentes por categoria", x = "Categoria", y = "Número de patentes")
+}
 
-before_2010 <- data.frame(table(before_2010$ano_desenvolvimento), stringsAsFactors = FALSE)
-colnames(before_2010) <- c('ano', 'qtd_patentes')
-
-after_2010 <- data.frame(table(after_2010$ano_desenvolvimento), stringsAsFactors = FALSE)
-colnames(after_2010) <- c('ano', 'qtd_patentes')
-
-means_years <- data.frame("year" = c("2002-2009","2010-2017"), "media" = c(mean(before_2010$qtd_patentes), mean(after_2010$qtd_patentes)))
-
-# Graph mean of years before and after 2010
-mean_year_bar <- ggplot(means_years, aes(x=year, y=media)) +
-  geom_bar(stat='identity', fill='#388E8E') +
-  labs(title = "Média de patentes antes e após 2010", x = "Ano", y = "Média do número de patentes")
-
-
-# By country graph
-country_df <- data.frame(table(result_after_2010$pais))
-colnames(country_df) <- c('pais', 'qtd_patentes')
-filtered_countries <- filter(country_df, qtd_patentes > 1 & pais != 'Brasil')
-country_bar <- ggplot(filtered_countries, aes(x=reorder(pais, qtd_patentes, sum), y=qtd_patentes)) +
-  coord_flip() +
-  scale_y_continuous(breaks = seq(0, 550, 25)) +
-  geom_bar(stat='identity', fill = '#0d627a') +
-  labs(title = "Patentes por países", x = "País", y = "Número de patentes")
-
-
-# By category
-pat_category <- data.frame(table(result_after_2010$categoria))
-colnames(pat_category) <- c('categoria', 'qtd_patentes')
-filtered_category <- filter(pat_category, qtd_patentes > 2 & categoria != "")
-category_bar <- ggplot(filtered_category, aes(x=categoria, y=qtd_patentes)) +
-  geom_bar(stat='identity', fill = '#0d627a') +
-  labs(title = "Patentes por categoria", x = "Categoria", y = "Número de patentes")
-
-
-# Key words cloud
-library(wordcloud2)
-
-keyword_df <- select(result_after_2010, palavra_chave_1, palavra_chave_2, 
-                     palavra_chave_3, palavra_chave_4, palavra_chave_5, palavra_chave_6)
-
-keyword_list <- unlist(keyword_df, use.names = FALSE)
-keyword_list <- keyword_list[keyword_list != ""]
-df_key <- data.frame(table(keyword_list))
-wordcloud_key <- wordcloud2(df_key)
-
-# By INCT
-df_incts <- data.frame(table(result_after_2010$inct))
-colnames(df_incts) <- c('inct', 'qtd_patentes')
+CloudKeyWords <- function(df) {
+  
+  # Key words cloud
+  library(wordcloud2)
+  
+  result_after_2010 <- FilterGreaterThan2010(df)
+  
+  keyword_df <- select(result_after_2010, palavra_chave_1, palavra_chave_2, 
+                       palavra_chave_3, palavra_chave_4, palavra_chave_5, palavra_chave_6)
+  
+  keyword_list <- unlist(keyword_df, use.names = FALSE)
+  keyword_list <- keyword_list[keyword_list != ""]
+  df_key <- data.frame(table(keyword_list))
+  wordcloud_key <- wordcloud2(df_key) 
+}
--- main.R
+++ main.R
@@ -11,8 +11,10 @@ source("read_cultivar_registrada.R")
 source("read_cultivar_protegida.R")
 source("read_desenho.R")
 source("merge_inct.R")
+source("graph_results.R")
 
 folder_path <- "~/Documentos/ds4all/gcee/teste/"
+incts_ids_path <- "~/Documentos/ds4all/gcee/data/Lista_IDLattes_patentes_membros_03102018.xlsx"
 
 df_pat <- ReadPatents(folder_path)
 df_marca <- ReadMarcas(folder_path)
@@ -21,4 +23,18 @@ df_software <- ReadSoftware(folder_path)
 df_cultivar_reg <- ReadCultivarRegistrada(folder_path)
 df_cultivar_proteg <-ReadCultivarProtegida(folder_path)
 df_desenho <- ReadDesenho(folder_path)
-df_merged <- MergeWithINCT(df_pat, "~/Documentos/ds4all/gcee/data/Lista_IDLattes_patentes_membros_03102018.xlsx")
+
+
+df_pat <- MergeWithINCT(df_pat, incts_ids_path)
+# df_marca <- MergeWithINCT(df_marca, incts_ids_path)
+# df_topo <- MergeWithINCT(df_topo, incts_ids_path)
+# df_software <- MergeWithINCT(df_software, incts_ids_path)
+# df_cultivar_reg <- MergeWithINCT(df_cultivar_reg, incts_ids_path)
+# df_cultivar_proteg <- MergeWithINCT(df_cultivar_proteg, incts_ids_path)
+# df_desenho <- MergeWithINCT(df_desenho, incts_ids_path)
+
+graph_by_year <- GraphByYear(df_pat)
+graph_by_mean_year <- GraphByMeanYear(df_pat)
+graph_by_country <- GraphByCountry(df_pat)
+graph_by_category <- GraphByCategory(df_pat)
+keywords_cloud <- CloudKeyWords(df_pat)
